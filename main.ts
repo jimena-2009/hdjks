46a48c47c9c9e7d42f14d044aec91392c17af235
jimena-2009 <jimena.gonzalez@panam.ed.cr>
2025-11-25 15:13:27 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,39 @@
+scene.onOverlapTile(SpriteKind.Player, sprites.castle.saplingOak, function (sprite, location) {
+	
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleRedCrystal, function (sprite, location) {
+    tiles.setCurrentTilemap(tilemap`level2`)
+    tiles.placeOnRandomTile(mySprite, sprites.dungeon.collectibleBlueCrystal)
+    scene.cameraFollowSprite(mySprite)
+    info.startCountdown(40)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleInsignia, function (sprite, location) {
+    tiles.setCurrentTilemap(tilemap`level3`)
+    tiles.placeOnRandomTile(mySprite, sprites.castle.shrub)
+    scene.cameraFollowSprite(mySprite)
+    info.startCountdown(30)
+})
+let mySprite: Sprite = null
+mySprite = sprites.create(img`
+    . . . 3 3 3 3 3 . . . . . . . . 
+    . . 3 3 3 3 3 3 3 . . . . . . . 
+    . . 3 3 3 3 3 3 3 3 3 3 . . . . 
+    . . 3 3 3 3 3 3 3 3 3 3 3 . . . 
+    . . 3 3 3 3 3 3 3 3 3 3 3 3 . . 
+    . . 3 3 3 3 3 3 3 3 3 3 3 3 . . 
+    . . . 3 3 3 3 3 3 3 3 3 3 3 . . 
+    . . . 3 3 3 3 3 3 3 3 3 3 3 . . 
+    . . . 3 3 3 3 3 3 3 3 3 3 3 . . 
+    . . . 3 3 3 3 3 3 3 3 3 3 3 . . 
+    . . . 3 3 3 3 3 3 3 3 3 3 3 . . 
+    . . . . 3 3 3 3 3 3 3 3 3 . . . 
+    . . . . . 3 3 3 3 3 3 3 . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite)
+tiles.setCurrentTilemap(tilemap`level1`)
+tiles.placeOnRandomTile(mySprite, sprites.dungeon.chestClosed)
+scene.cameraFollowSprite(mySprite)
+info.startCountdown(60)
